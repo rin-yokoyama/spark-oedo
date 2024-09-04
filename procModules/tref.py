8ba09c87c4d3430e75c4a3da951f3c0d803a874f
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2024-09-04 16:16:38 +0900

    tref edge == 0 from 1

--- procModules/tref.py
+++ procModules/tref.py
@@ -4,6 +4,6 @@ from procModules import mapper, constants
 
 def Tref(spark: "SparkSession", dataFrame: "F.DataFrame") -> F.DataFrame:
     mapdf = spark.read.csv(f"hdfs://"+constants.CLUSTER_NAME+":9000"+constants.MAPFILE_PATH+"/tref.csv", header=True, inferSchema=True).cache()
-    df_tref = mapper.Map(spark, dataFrame, "tref", mapdf).filter("edge == 1")
+    df_tref = mapper.Map(spark, dataFrame, "tref", mapdf).filter("edge == 0")
     
     return F.broadcast(df_tref)
